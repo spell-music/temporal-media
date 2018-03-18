4cd4f8502056fec946a3ae94e084093c6f39a058
vivid-synth <vivid.haskell@gmail.com>
2018-03-18 14:33:15 -0400

    Replace tabs with spaces

--- src/Temporal/Class.hs
+++ src/Temporal/Class.hs
@@ -1,37 +1,37 @@
 {-# Language TypeFamilies #-}
 module Temporal.Class(
-	DurOf, Duration(..), Melody(..), Harmony(..), Compose(..), 
-	loopBy, melMap, harMap, 
-	Delay(..), (+|),
-	Rest(..), Stretch(..), (*|), Limit(..), 
-	Loop(..)
+   DurOf, Duration(..), Melody(..), Harmony(..), Compose(..),
+   loopBy, melMap, harMap,
+   Delay(..), (+|),
+   Rest(..), Stretch(..), (*|), Limit(..),
+   Loop(..)
 ) where
 
 -- | Calculates duration.
 class Duration a where
-	dur :: a -> DurOf a
+   dur :: a -> DurOf a
 
 -- | Duration for the given type.
 type family DurOf a :: *
 
 class Melody a where
-	-- | Sequent composition for a list of values (melody). 
-	mel :: [a] -> a
-	-- | Sequent composition. Play first track then second.
-	(+:+) :: a -> a -> a
+   -- | Sequent composition for a list of values (melody).
+   mel :: [a] -> a
+   -- | Sequent composition. Play first track then second.
+   (+:+) :: a -> a -> a
 
-	a +:+ b = mel [a, b]
-	mel = foldl1 (+:+)
+   a +:+ b = mel [a, b]
+   mel = foldl1 (+:+)
 
 class Harmony a where
-	-- | Parallel composition for a list of values (harmony). 
-	har :: [a] -> a
+   -- | Parallel composition for a list of values (harmony).
+   har :: [a] -> a
 
-	-- | Parallel composition. Play two tracks simultaneously.
-	(=:=) :: a -> a -> a
+   -- | Parallel composition. Play two tracks simultaneously.
+   (=:=) :: a -> a -> a
 
-	a =:= b = har [a, b]
-	har = foldl1 (=:=)
+   a =:= b = har [a, b]
+   har = foldl1 (=:=)
 
 
 class (Melody a, Harmony a) => Compose a where
@@ -41,12 +41,12 @@ loopBy :: Melody a => Int -> a -> a
 loopBy n = mel . replicate n
 
 class Delay a where
-	-- | Delays the sound source by the given duration.
-	del :: DurOf a -> a -> a
+   -- | Delays the sound source by the given duration.
+   del :: DurOf a -> a -> a
 
 class Stretch a where
-	-- | Delays the sound source by the given duration factor.
-	str :: DurOf a -> a -> a
+   -- | Delays the sound source by the given duration factor.
+   str :: DurOf a -> a -> a
 
 -- | Infix 'del' function.
 (+|) :: Delay a => DurOf a -> a -> a
@@ -57,15 +57,15 @@ class Stretch a where
 (*|) = str
 
 class Rest a where
-	rest :: DurOf a -> a
+   rest :: DurOf a -> a
 
 class Limit a where
-	-- | Limits the duration of the sound source.
-	lim :: DurOf a -> a -> a
+   -- | Limits the duration of the sound source.
+   lim :: DurOf a -> a -> a
 
 class Loop a where
-	-- | Loops over the sound
-	loop :: a -> a
+   -- | Loops over the sound
+   loop :: a -> a
 
 -- | Transforms a sequence and then applies a mel.
 melMap :: (Melody b) => (a -> b) -> [a] -> b
